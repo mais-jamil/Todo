00964de92a7d48822b28cb48e0feaddc4f500bd9
Mais B <maisjamil90@gmail.com>
2019-09-23 15:36:04 +0300

    create build file

---
+++ build/precache-manifest.ab76523b498d496f1cb6f77070b3bd3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03c0890fa85668a2d2190142829b6f3e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a792f3670b49f2813bf3",
+    "url": "/static/css/main.07f8d3d7.chunk.css"
+  },
+  {
+    "revision": "1a7994dc6f9847088023",
+    "url": "/static/js/2.94180839.chunk.js"
+  },
+  {
+    "revision": "a792f3670b49f2813bf3",
+    "url": "/static/js/main.89f9105e.chunk.js"
+  },
+  {
+    "revision": "1459d5785bc55432c401",
+    "url": "/static/js/runtime~main.2dc77212.js"
+  },
+  {
+    "revision": "91186db895792b90e75dd536a80dc95e",
+    "url": "/static/media/preview.91186db8.png"
+  }
+]);
