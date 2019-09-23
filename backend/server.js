429cd589c5e850079bda95f0178af80749c6729b
Mais B <maisjamil90@gmail.com>
2019-09-23 15:28:23 +0300

    add eviroment to prot

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const PORT =5050
+const PORT =  process.env.PORT || 5050;
 const mongoose = require('mongoose')
 const Todo = require('./Module/todoList')
 const signupUser = require('./Module/signupUserSchema')
